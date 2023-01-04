3ec2468740102fe09fb30f1283ec1d503ac703f3
Taylor <hillking2017@gmail.com>
2023-01-04 03:36:49 -0800

    messing with adding audio in a click event

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length
 
+var audio = new Audio('sounds/tom-1.mp3');
+
 for (var i = 0; i<numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("I got clicked!");
+        audio.play();
     });
 }
 
