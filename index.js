3ae858c542e71a60b7604711b6e1f2cc0ad3c283
Taylor <hillking2017@gmail.com>
2023-01-04 16:47:42 -0800

    added makeSound function and then passed ths is function to event listeners

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
+// This detects when you click on a drum button.
 var numberOfDrumButtons = document.querySelectorAll(".drum").length
 
 for (var i = 0; i<numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         
         var buttonInnerHTML = this.innerHTML;
-        
-        switch (buttonInnerHTML) {
-            case "w":
-                var tom1 = new Audio('sounds/tom-1.mp3');
-                tom1.play();
-                break;
-            case "a":
-                var tom2 = new Audio('sounds/tom-2.mp3');
-                tom2.play();
-                break;
-            case "s":
-                var tom3 = new Audio('sounds/tom-3.mp3');
-                tom3.play();
-                break;
-            case "d":
-                var tom4 = new Audio('sounds/tom-4.mp3');
-                tom4.play();
-                break;
-            case "j":
-                var crash = new Audio('sounds/crash.mp3');
-                crash.play();
-                break;
-            case "k":
-                var kick = new Audio('sounds/kick-bass.mp3');
-                kick.play();
-                break;
-            case "l":
-                var snare = new Audio('sounds/snare.mp3');
-                snare.play();
-                break;
-            default:
-                console.log(buttonInnerHTML)
-                break;
-        }
+
+        makeSound(buttonInnerHTML);
     });
 }
+
+// This detects when you press the key on the keyboard.
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+});
+
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "k":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case "l":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        default:
+            console.log(buttonInnerHTML)
+            break;
+    }
+};
 
 // Below is a more advanced way of doing the above with forEach and arrow functions.
 // let buttons = document.querySelectorAll("button");
